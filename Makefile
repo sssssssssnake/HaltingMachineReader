e55ac073cf6b6d52df50f0d7b93c690f89e2c177
sssssssssnake <samuel@mulinoit.com>
2024-06-21 16:22:59 -0700

    Refactor Makefile to separate file management logic into a separate module

--- Makefile
+++ Makefile
@@ -9,10 +9,12 @@ run: build
 build: clean
 	@# compile the program
 	@echo "Compiling the program..."
-	@gfortran -o program src/main.f95
+	@gfortran -c src/filemanage.f95
+	@gfortran -o program src/main.f95 filemanage.o
 
 clean:
 	@# if the file exists, remove it
 	@if [ -f program ]; then rm program; fi
 	@if [ -f *.mod ]; then rm *.mod; fi
+	@if [ -f *.o ]; then rm *.o; fi
 	@if [ -f *.10 ]; then rm *.10; fi
--- src/main.f95
+++ src/main.f95
@@ -1,49 +1,4 @@
-module fileReader
-    implicit none
 
-
-    
-    contains
-    subroutine readFile(filePath, fileContents, endingLine)
-        character(:), allocatable, intent(in) :: filePath
-        character(:), allocatable, intent(out) :: fileContents(:)
-        integer, intent(out) :: endingLine
-
-        integer :: counter, fileSize, fileUnit, iostat
-
-        fileUnit = 10
-
-        open(newunit=fileUnit, file=filePath, status='old', action='read', iostat=iostat)
-        inquire(fileUnit, size=fileSize)
-        print *, "fileSize: ", fileSize
-        call printFileStatus(iostat)
-
-        ! allocate the worst case scenarios at the same time
-        ! There could be one long line in the file or many one character lines
-        allocate(character(fileSize) :: fileContents(fileSize))
-
-        counter = 1
-        do counter = 1, 100
-            read(fileUnit, '(A)', iostat=iostat) fileContents(counter)
-            if ( iostat /= 0 ) then
-                endingLine = counter - 1
-                exit
-            end if
-        end do
-
-
-
-    end subroutine readFile
-
-    subroutine printFileStatus(status)
-        integer, intent(in) :: status
-        if ( status == 0 ) then
-            print *, "File status: OK"
-        else
-            print *, "File status: ", status
-        end if
-    end subroutine printFileStatus
-end module fileReader
 
 program HelloWorld
     use fileReader
@@ -56,7 +11,7 @@ program HelloWorld
     
 
     ! Main program
-    myFilePath = "main.f95"
+    myFilePath = "src/main.f95"
 
     call readFile(myFilePath, myFileContents, endingLine)
     
