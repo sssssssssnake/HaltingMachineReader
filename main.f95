fb966b3736c891a8d495c2aa9c9151f2235e42e8
sssssssssnake <samuel@mulinoit.com>
2024-06-21 14:02:05 -0700

    it can compile now!

--- main.f95
+++ main.f95
@@ -1,30 +1,33 @@
 program HelloWorld
     implicit none
     character(:), allocatable :: myFilePath
-    character(:), allocatable :: myFileContents
+    character(:), allocatable :: myFileContents(:, :)
     integer :: index
 
     ! interface for readFile subroutine
     interface
         subroutine readFile(filePath, fileContents)
             character(:), allocatable, intent(in) :: filePath
-            character(:), allocatable, intent(out) :: fileContents
+            character(:), allocatable, intent(out) :: fileContents (:, :)
+                    
         end subroutine readFile
     end interface
 
     ! Main program
     print *, "Hello, World!"
     myFilePath = "main.f95"
-    call readFile(myFilePath, myFileContents)
-    print *, myFileContents
-    print *, len(myFileContents)
-    ! do index = 1, 1000
-    !     if ( myFileContents(index:index) == "" ) then
-    !         print *, "End of file"
-    !     else
-    !         print *, myFileContents(index:index)
-    !     end if
-    ! end do
+
+    ! call readFile(myFilePath, myFileContents)
+    ! print *, myFileContents
+    ! print *, len(myFileContents)
 
 end program HelloWorld
 
+subroutine readFile(filePath, fileContents)
+    character(:), allocatable, intent(in) :: filePath
+    character(:), allocatable, intent(out) :: fileContents(:, :)
+
+    
+
+
+end subroutine readFile
