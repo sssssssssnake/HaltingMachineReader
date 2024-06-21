b3132761028e5ba07baffaa841cedfe942862190
sssssssssnake <samuel@mulinoit.com>
2024-06-21 12:16:29 -0700

    fun

--- Makefile
+++ Makefile
@@ -1,12 +1,11 @@
-
 default: run
 
 run: build
-	./program
+	@./program
 
 build: clean
-	gfortran -o program main.f95
+	@gfortran -o program main.f95
 
 clean:
-	# if the file exists, remove it
-	if [ -f program ]; then rm program; fi
+	@# if the file exists, remove it
+	@if [ -f program ]; then rm program; fi
--- main.f95
+++ main.f95
@@ -1,6 +1,19 @@
 program HelloWorld
     implicit none
+    character(:), allocatable :: myFilePath
+    character(:), allocatable :: myFileContents
     ! Main program
     print *, "Hello, World!"
-    
+    myFilePath = "/home/sssssssssnake/Documents/FunProjects/FortranHalting/main.f95"
+
 end program HelloWorld
+
+
+subroutine readFile(filePath, fileContents)
+    character(:),allocatable, intent(in) :: filePath
+    character(:), allocatable, intent(out) :: fileContents
+    ! now to read the file
+    open(10, file=filePath, status='old', action='read')
+
+    
+end subroutine readFile
