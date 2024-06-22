34efcdf6069d743806b54da5d1cc04d78aa5a69e
sssssssssnake <samuel@mulinoit.com>
2024-06-21 20:01:42 -0700

    feat: Add boolean test and print its value in Main.java

--- javacode/Main.java
+++ javacode/Main.java
@@ -6,5 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Hello, World!");
+        boolean test = true;
+        if (test) {
+            test = false;
+        }
+        System.out.println(test);
     }
 }
