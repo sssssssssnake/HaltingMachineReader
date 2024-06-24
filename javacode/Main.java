877b6fbd24a9f1db139cc1a4283aabeb770851b7
sssssssssnake <samuel@mulinoit.com>
2024-06-23 17:04:25 -0700

    add new test code that shoule be challenging

--- javacode/Main.java
+++ javacode/Main.java
@@ -3,12 +3,13 @@ package javacode;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Arrays.*; // Import all classes in the package
+import javacode.coolpackage.RandomLogic; // Import all classes in the package
 public class Main {
     // Main method
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         List<String> list = new ArrayList<>();
+        RandomLogic inner = new RandomLogic();
 
         System.out.println("Hello, World!");
         boolean test = true;
---
+++ javacode/coolpackage/RandomLogic.java
@@ -0,0 +1,15 @@
+package javacode.coolpackage;
+
+
+
+
+public class RandomLogic {
+    
+    class InnerClass {
+        public void print() {
+            System.out.println("Inner class");
+        }
+        Integer i = 0;
+    }
+
+}
